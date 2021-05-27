a00c888a7f039af1e305f6dbf8cc5af50ae17499
Atom1cJay <thegramps101@gmail.com>
2021-05-27 04:15:36 -0400

    CHARACTER ANIMATIONS BABY!!!

---
+++ Assets/PlayerAnimationHandler.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationHandler : MonoBehaviour
+{
+    [SerializeField] MovementMaster mm;
+    [SerializeField] Animator animator;
+
+    void Start()
+    {
+        //Initialize all Animation variables as false
+        animator.SetBool("JUMPING", false);
+        animator.SetBool("WALKING", false);
+        animator.SetBool("RUNNING", false);
+        animator.SetBool("FALLING", false);
+    }
+
+    void FixedUpdate()
+    {
+
+        Debug.Log(mm.GetHorizSpeed());
+        //Jumping and Falling Animation Controllers
+        if (mm.IsJumping())
+        {
+            animator.SetBool("JUMPING", true);
+            animator.SetBool("FALLING", false);
+        }
+        else if (!mm.IsJumping() && !mm.IsOnGround())
+        {
+            animator.SetBool("JUMPING", false);
+            animator.SetBool("FALLING", true);
+        }
+        else
+        {
+            animator.SetBool("JUMPING", false);
+            animator.SetBool("FALLING", false);
+        }
+
+        //Walking Animation Controller
+        if(mm.GetHorizSpeed() > 3f)
+        {
+            animator.SetBool("WALKING", false);
+            animator.SetBool("RUNNING", true);
+        }
+        else if (mm.GetHorizSpeed() > 0.4f)
+        {
+
+            animator.SetBool("WALKING", true);
+            animator.SetBool("RUNNING", false);
+        }
+        else
+        {
+            animator.SetBool("WALKING", false);
+            animator.SetBool("RUNNING", false);
+        }
+    }
+}
