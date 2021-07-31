be51bcc21b91ac16cc3e5dc7d95826c163a0ab2b
JelindoGames <51799620+JelindoGames@users.noreply.github.com>
2021-07-30 22:22:42 -0700

    Stuffs

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -8,20 +8,27 @@ public class MovingPlatform : MonoBehaviour
     [SerializeField] float speed;
     [SerializeField] Vector3 rotSpeed;
     [SerializeField] float timeOffset;
-    Vector3 distToMove;
-    Vector3 amtToRot;
+    [SerializeField] bool movementIsRelative;
+    Vector3 mvmtThisFrame;
+    Vector3 rotThisFrame;
+
+    private void Start()
+    {
+        transform.Rotate(rotSpeed * timeOffset);
+        transform.position += (GetDistanceToMove() * (Mathf.Sin(speed * timeOffset) / 2));
+    }
 
     void Update()
     {
         float waveValue = speed * (Mathf.Sin(speed * (Time.time + timeOffset)) / 2);
 
-        distToMove =
+        mvmtThisFrame =
             new Vector3(
-                waveValue * distanceToMove.x,
-                waveValue * distanceToMove.y,
-                waveValue * distanceToMove.z)* Time.fixedDeltaTime;
+                waveValue * GetDistanceToMove().x,
+                waveValue * GetDistanceToMove().y,
+                waveValue * GetDistanceToMove().z)* Time.fixedDeltaTime;
 
-        amtToRot =
+        rotThisFrame =
             new Vector3(
                 rotSpeed.x,
                 rotSpeed.y,
@@ -30,7 +37,22 @@ public class MovingPlatform : MonoBehaviour
 
     void FixedUpdate()
     {
-        transform.Translate(distToMove);
-        transform.Rotate(amtToRot);
+        transform.Translate(mvmtThisFrame);
+        transform.Rotate(rotThisFrame);
+    }
+
+    /// <summary>
+    /// Gives distanceToMove as an absolute vector, but if movement is relative,
+    /// it takes into consideration the current angle of this object.
+    /// </summary>
+    Vector3 GetDistanceToMove()
+    {
+        if (!movementIsRelative)
+        {
+            return distanceToMove;
+        }
+        return transform.right * distanceToMove.x
+            + transform.up * distanceToMove.y
+            + transform.forward * distanceToMove.z;
     }
 }
