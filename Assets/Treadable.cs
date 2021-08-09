8b0ed0816b563d96af08af152daa2c9e1ff9ef47
JelindoGames <51799620+JelindoGames@users.noreply.github.com>
2021-08-08 20:45:47 -0700

    Treadable Functioning

--- Assets/Treadable.cs
+++ Assets/Treadable.cs
@@ -3,15 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
+/// <summary>
+/// NOTE: This class needs to be put on the HOLDER for whatever platform is
+/// treadable. Said platform also needs to be marked as a moving platform.
+/// </summary>
 public class Treadable : MonoBehaviour
 {
     [SerializeField] UnityEvent OnTread;
 
+    private void FixedUpdate()
+    {
+        if (transform.Find("Player") != null)
+        {
+            BroadcastTreadEvent();
+        }
+    }
+
     /// <summary>
     /// Calls the specific event to occur when this object is treaded on.
     /// </summary>
-    public void BroadcastTreadEvent()
+    private void BroadcastTreadEvent()
     {
+        print("fuck");
         OnTread.Invoke();
     }
 }
