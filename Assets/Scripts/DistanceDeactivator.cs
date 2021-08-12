f06fc47f4ddd1030daf7b9adc8e43b71253d2b76
JelindoGames <51799620+JelindoGames@users.noreply.github.com>
2021-08-11 19:51:20 -0700

    Distance Deactivation

---
+++ Assets/Scripts/DistanceDeactivator.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistanceDeactivator : MonoBehaviour
+{
+    [SerializeField] MonoBehaviour toDeactivate;
+    [SerializeField] float deactivateDistance;
+
+    private void Start()
+    {
+        DistanceDeactivatorHandler.instance.Register(this);
+    }
+
+    /// <summary>
+    /// Based on the DistanceDeactivatorHandler's position, is this object so
+    /// far away that it should deactivate?
+    /// </summary>
+    public void HandleDistance(Vector3 ddhPos)
+    {
+        if (toDeactivate.enabled && Vector3.Distance(ddhPos, transform.position) > deactivateDistance)
+        {
+            toDeactivate.enabled = false;
+        }
+        else if (!toDeactivate.enabled && Vector3.Distance(ddhPos, transform.position) < deactivateDistance)
+        {
+            toDeactivate.enabled = true;
+        }
+    }
+}
