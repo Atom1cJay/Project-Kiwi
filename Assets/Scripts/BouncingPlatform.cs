046852159c57c0fdec37a00c0f4934303bdde2f3
Atom1cJay <thegramps101@gmail.com>
2021-08-25 23:22:57 -0400

    Everything

--- Assets/Scripts/BouncingPlatform.cs
+++ Assets/Scripts/BouncingPlatform.cs
@@ -6,6 +6,11 @@ public class BouncingPlatform : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] float speed, randomRange;
+    [SerializeField] bool maintainAngle;
+    [SerializeField] Transform AngleTransform;
+    [SerializeField] GameObject bouncingCollider;
+    [SerializeField] Vector3 correctingVector;
+
     Vector3 mvmtVector;
 
     void Awake()
@@ -17,6 +22,16 @@ public class BouncingPlatform : MonoBehaviour
             random = new Vector3(Random.Range(-1f, 1f), 0f, Random.Range(-1f, 1f));
 
         mvmtVector = random.normalized * speed;
+
+        if (maintainAngle)
+        {
+            //t.eulerAngles = new Vector3(t.eulerAngles.x, Vector3.Angle(Vector3.zero, mvmtVector), t.eulerAngles.z);
+
+            AngleTransform.rotation = Quaternion.LookRotation(mvmtVector);
+            AngleTransform.eulerAngles = new Vector3(correctingVector.x, AngleTransform.eulerAngles.y + correctingVector.y, correctingVector.z);
+
+        }
+
     }
 
     private void FixedUpdate()
@@ -25,17 +40,45 @@ public class BouncingPlatform : MonoBehaviour
     }
     private void OnCollisionEnter(Collision other)
     {
-        if (other.gameObject.layer != 9)
+        if (other.gameObject == bouncingCollider)
         {
-            Debug.Log(other.gameObject.name);
+
+            Vector3 normal = other.GetContact(0).normal;
+            normal = new Vector3(normal.x, 0f, normal.z);
+            Vector3 random = new Vector3(Random.Range(-randomRange, randomRange), 0f, Random.Range(-randomRange, randomRange));
+            Debug.Log("ookachaka");
+            mvmtVector = (normal + random).normalized * speed;
+            if (maintainAngle)
+            {
+                //t.eulerAngles = new Vector3(t.eulerAngles.x, Vector3.Angle(Vector3.zero, mvmtVector), t.eulerAngles.z);
+
+                AngleTransform.rotation = Quaternion.LookRotation(mvmtVector);
+                AngleTransform.eulerAngles = new Vector3(correctingVector.x, AngleTransform.eulerAngles.y + correctingVector.y, correctingVector.z);
+                AngleTransform.transform.position = AngleTransform.transform.position + mvmtVector * 0.125f;
+            }
+
+        }
+    }
+    private void OnCollisionStay(Collision other)
+    {
+        if (other.gameObject == bouncingCollider)
+        {
 
             Vector3 normal = other.GetContact(0).normal;
             normal = new Vector3(normal.x, 0f, normal.z);
             Vector3 random = new Vector3(Random.Range(-randomRange, randomRange), 0f, Random.Range(-randomRange, randomRange));
 
-
             mvmtVector = (normal + random).normalized * speed;
 
+            if (maintainAngle)
+            {
+                AngleTransform.rotation = Quaternion.LookRotation(mvmtVector);
+                AngleTransform.eulerAngles = new Vector3(correctingVector.x, AngleTransform.eulerAngles.y + correctingVector.y, correctingVector.z);
+                AngleTransform.transform.position = AngleTransform.transform.position + mvmtVector * 0.125f;
+
+            }
+            else
+                transform.parent.position = transform.parent.position + mvmtVector * 0.5f;
         }
     }
 }
