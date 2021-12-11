e688bf0cbbed6fe84871b6df04bfebfb40191f1c
JelindoGames <51799620+JelindoGames@users.noreply.github.com>
2021-12-11 14:14:18 -0500

    Infrastructure for Shadow Damage

---
+++ Assets/Scripts/Health & Damage/_SpecialDamagers/ShadowDamager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Damages the player if they are standing on a shadow.
+public class ShadowDamager : MonoBehaviour
+{
+    [SerializeField] DamageType damageType;
+    [SerializeField] PlayerHealth ph;
+    [SerializeField] MovementInfo mi;
+    [SerializeField] Transform player;
+    [SerializeField] LayerMask raycastLayerMask;
+    [SerializeField] Transform directionalLight;
+
+    void Update()
+    {
+        Debug.DrawRay(player.position, -directionalLight.transform.forward);
+        if (mi.TouchingGround())
+        {
+            if (Physics.Raycast(player.position, -directionalLight.transform.forward, 100, raycastLayerMask))
+            {
+                print("Hitting Shadow");
+            }
+        }
+    }
+}
