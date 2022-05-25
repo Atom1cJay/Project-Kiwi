c8be628d5545951831491ffcaf8bf13a172d702a
Atom1cJay <thegramps101@gmail.com>
2022-05-25 01:15:50 -0400

    CheckPoint!

---
+++ Assets/CheckpointVisualLoader.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointVisualLoader : MonoBehaviour
+{
+    [SerializeField] Animator animator;
+
+    [SerializeField] GameObject visual, tempVisual;
+    [SerializeField] float timeToDisplay;
+    [SerializeField] ParticleSystem PS;
+
+
+    private void Start()
+    {
+        startVisuals();
+    }
+
+    public void startVisuals()
+    {
+        animator.SetBool("START", true);
+        tempVisual.SetActive(false);
+    }
+
+    public void finishAnimation()
+    {
+        animator.SetBool("START", false);
+        Invoke("displayPlayer", timeToDisplay);
+    }
+
+    public void startParticles()
+    {
+        PS.Play();
+    }
+
+    void displayPlayer()
+    {
+        tempVisual.SetActive(true);
+    }
+}
--- Assets/Scripts/Checkpoint/CheckpointSystem.cs
+++ Assets/Scripts/Checkpoint/CheckpointSystem.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class CheckpointSystem : MonoBehaviour
 {
-    [SerializeField] Material currentCheckpointMat,inactiveCheckpointMat;
     CheckpointLoader cL;
-    [SerializeField] MeshRenderer mr;
+    [SerializeField] GameObject[] rings;
+    [SerializeField] ParticleSystem[] PS;
+    [SerializeField] Rotator[] rotators;
 
     void Awake()
     {
-        mr = GetComponent<MeshRenderer>();
         SetInactive();
 
     }
@@ -24,13 +24,66 @@ public class CheckpointSystem : MonoBehaviour
     //Set checkpoint to inactive
     public void SetInactive()
     {
-        mr.material = inactiveCheckpointMat;
+        StartCoroutine(SetInactiveScaling());
+
+        foreach (ParticleSystem ps in PS)
+        {
+            ps.Stop();
+        }
+
+        foreach (Rotator rot in rotators)
+        {
+            rot.enabled = false;
+        }
+    
     }
 
     //Set checkpoint to active
     public void SetActive()
     {
-        mr.material = currentCheckpointMat;
+        StartCoroutine(SetActiveScaling());
+
+        foreach (ParticleSystem ps in PS)
+        {
+            ps.Play();
+        }
+
+        foreach (Rotator rot in rotators)
+        {
+            rot.enabled = true;
+        }
+    }
+
+    IEnumerator SetActiveScaling()
+    {
+        Debug.Log("Set Active!");
+        GameObject ring = rings[0];
+
+        while (ring.transform.localScale.magnitude < 1.732f)
+        {
+
+            foreach (GameObject g in rings)
+            {
+                g.transform.localScale = Vector3.Lerp(g.transform.localScale, Vector3.one, Time.deltaTime);
+            }
+            yield return new WaitForSeconds(Time.deltaTime);
+        }
+    }
+
+    IEnumerator SetInactiveScaling()
+    {
+        Debug.Log("Setawd Active!");
+        GameObject ring = rings[0];
+
+        while (ring.transform.localScale.magnitude > 1.311f)
+        {
+
+            foreach (GameObject g in rings)
+            {
+                g.transform.localScale = Vector3.Lerp(g.transform.localScale, new Vector3(0.6f, 1f, 0.6f), Time.deltaTime);
+            }
+            yield return new WaitForSeconds(Time.deltaTime);
+        }
     }
 
     //get position of checkpoint
