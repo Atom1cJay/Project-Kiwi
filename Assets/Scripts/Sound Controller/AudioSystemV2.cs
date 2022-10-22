e995f3555b51cbff3500336b3c0cbdf09d6a20eb
JelindoGames <51799620+JelindoGames@users.noreply.github.com>
2022-10-21 23:04:24 -0700

    Some Sounds

--- Assets/Scripts/Sound Controller/AudioSystemV2.cs
+++ Assets/Scripts/Sound Controller/AudioSystemV2.cs
@@ -7,17 +7,15 @@ public class AudioSystemV2 : MonoBehaviour
     TransitionCheck[] controllers;
     [SerializeField] MoveExecuter me;
     string lastMove = "";
-    // Start is called before the first frame update
+
     void Awake()
     {
         controllers = GetComponentsInChildren<TransitionCheck>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         string currentMove = me.GetCurrentMove().AsString();
-        //Debug.Log(currentMove);
         foreach (TransitionCheck c in controllers)
         {
             c.UpdateMoves(lastMove, currentMove);
