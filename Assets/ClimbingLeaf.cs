61c3008de7e36c4d13289f91ce2101a847725597
JWeinstein <74260546+Atom1cJay@users.noreply.github.com>
2024-02-15 02:12:20 -0500

    Climbing Tree

---
+++ Assets/ClimbingLeaf.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClimbingLeaf : MonoBehaviour
+{
+    [SerializeField] Animator leafAnimator;
+    [SerializeField] Collider collider;
+    [SerializeField] ClimbingLeaf[] leafsToChain;
+    [SerializeField] float timeToStayUp;
+    [SerializeField] float timeToChain;
+    [SerializeField] float timeToWaitAtStart;
+    [SerializeField] float animationLag;
+
+    bool leafUp = false;
+
+    public void startLeaf()
+    {
+        StopAllCoroutines();
+        StartCoroutine(leafProcess());
+
+        if (leafsToChain.Length > 0)
+            Invoke("chainLeaves", timeToChain);
+    }
+
+    IEnumerator leafProcess()
+    {
+        yield return new WaitForSeconds(timeToWaitAtStart);
+
+        if (!leafUp)
+        {
+            leafUp = true;
+            leafAnimator.SetTrigger("RISE");
+        }
+
+        collider.enabled = true;
+
+        yield return new WaitForSeconds(timeToStayUp);
+
+        leafAnimator.SetTrigger("FALL");
+
+        yield return new WaitForSeconds(animationLag);
+
+        collider.enabled = false;
+        leafUp = false;
+    }
+
+    void chainLeaves()
+    {
+        foreach (ClimbingLeaf leaf in leafsToChain)
+            leaf.startLeaf();
+    }
+
+}
