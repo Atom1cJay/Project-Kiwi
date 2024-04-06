14a2e613180b0ff904f87f4295e9751fc2d7efb3
JelindoGames <51799620+JelindoGames@users.noreply.github.com>
2024-04-06 12:50:31 -0400

    Overhaul collectableGroup system

--- Assets/Scripts/Collectible/CollectibleGroup.cs
+++ Assets/Scripts/Collectible/CollectibleGroup.cs
@@ -5,45 +5,25 @@ using UnityEngine.Events;
 
 public class CollectibleGroup : MonoBehaviour
 {
-    static Dictionary<string, int> liveCollectables = new Dictionary<string, int>();
-    static Dictionary<string, int> numInGroup = new Dictionary<string, int>();
-    [SerializeField] string id;
+    [SerializeField] CollectibleReader[] collectables;
     [SerializeField] UnityEvent<int, int> onCollected; // arg1 = num collected so far, arg2 = total in group
     [SerializeField] UnityEvent onGroupCollected;
-    bool isCollected; // Just in case--make sure double collect shouldn't happen (it shouldn't happen in the first place)
-
-    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
-    static void InitStatics()
-    {
-        liveCollectables = new Dictionary<string, int>();
-        numInGroup = new Dictionary<string, int>();
-    }
+    int remaining;
 
     void Start()
     {
-        CollectibleReader reader = GetComponent<CollectibleReader>();
-        reader.onCollect.AddListener(OnCollected);
-        if (!liveCollectables.ContainsKey(id))
+        remaining = collectables.Length;
+        foreach (CollectibleReader reader in collectables)
         {
-            liveCollectables[id] = 1;
-            numInGroup[id] = 1;
-        }
-        else
-        {
-            liveCollectables[id] += 1;
-            numInGroup[id] += 1;
+            reader.onCollect.AddListener(OnCollected);
         }
     }
 
     void OnCollected()
     {
-        if (!isCollected)
-        {
-            isCollected = true;
-            liveCollectables[id] -= 1;
-            onCollected.Invoke(numInGroup[id] - liveCollectables[id], numInGroup[id]);
-        }
-        if (liveCollectables[id] == 0)
+        remaining -= 1;
+        onCollected.Invoke(collectables.Length - remaining, collectables.Length);
+        if (remaining == 0)
         {
             onGroupCollected.Invoke();
             print("Whole group is dead");
